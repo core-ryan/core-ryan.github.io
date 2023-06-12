54d5124aff076db8e408a55bc3a0ad23921a06a5
core-ryan <ryanjr0620@yahoo.com>
2023-06-12 00:17:30 -0600

    Removed fade in for icons on the left of page

--- js/script.js
+++ js/script.js
@@ -15,5 +15,5 @@ const sr = ScrollReveal ({
 
 sr.reveal('.main-text',{delay:200, origin:'top'});
 sr.reveal('.main-image',{delay:450, origin:'top'});
-sr.reveal('.icons',{delay:500, origin:'left'});
+// sr.reveal('.icons',{delay:500, origin:'left'});
 sr.reveal('.scroll-down',{delay:500, origin:'right'});
